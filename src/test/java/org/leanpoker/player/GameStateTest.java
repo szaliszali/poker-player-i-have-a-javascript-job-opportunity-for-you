0409216522cd8796d44133d6f4e7f2e2adf39507
Szali Szali <szalicil@gmail.com>
2020-04-04 12:20:26 +0200

    can i has a test?

---
+++ src/test/java/org/leanpoker/player/GameStateTest.java
@@ -0,0 +1,11 @@
+package org.leanpoker.player;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameStateTest {
+    @Test
+    public void aTest() {
+        Assertions.assertTrue(1==1);
+    }
+}
